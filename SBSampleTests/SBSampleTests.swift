6e46812f318cfff739670c558817a788432d0eef
Durk-jae Yun <durkjae.yun@directionsoft.com>
2024-10-16 10:03:05 +0900

    Initial Commit

---
+++ SBSampleTests/SBSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SBSampleTests.swift
+//  SBSampleTests
+//
+//  Created by Durk Jae Yun on 10/16/24.
+//
+
+import Testing
+@testable import SBSample
+
+struct SBSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ SBSampleUITests/SBSampleUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SBSampleUITestsLaunchTests.swift
+//  SBSampleUITests
+//
+//  Created by Durk Jae Yun on 10/16/24.
+//
+
+import XCTest
+
+final class SBSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
